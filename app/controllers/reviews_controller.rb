be6c50113d9d78f13c7eb6e4fced089c63e53e2c
tom.debek@gmail.com <tom.debek@gmail.com>
2019-06-05 22:56:18 +0200

    Finished Reviews :)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,11 +20,9 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
       end
     end
   end
